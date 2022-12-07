d2ed4c5e7eba5b8bdaeeab31cc0dfbd7db8d7df0
MKAbuMattar <mohammad.khaled@outlook.com>
2022-12-07 19:41:00 +0300

    build: index.php text change

---
+++ app/controllers/index.php
@@ -0,0 +1,13 @@
+<?php
+
+// Include the connection.php file
+require_once '../config/connection.php';
+
+// Get the data from the database
+$query = "SELECT * FROM `users`";
+$stmt = $conn->prepare($query);
+$stmt->execute();
+$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// close the connection
+$conn = null;
---
+++ app/views/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Quick Test Jenkins</title>
+  </head>
+  <body>
+    <h1>Quick Test Jenkins</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($users as $user): ?>
+          <tr>
+            <td><?php echo $user['id']; ?></td>
+            <td><?php echo $user['name']; ?></td>
+            <td><?php echo $user['email']; ?></td>
+          </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+  </body>
+</html>
