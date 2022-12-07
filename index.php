67a1bfd841ddc9e2ed30ed742378df89a6b97671
MKAbuMattar <mohammad.khaled@outlook.com>
2022-12-07 18:52:39 +0300

    build: index.php text change

--- index.php
+++ index.php
@@ -1,7 +1,40 @@
 <?php
+require_once 'config/env.php';
+require_once 'config/connection.php';
+?>
 
-// Include the controller file
-require_once 'app/controllers/index.php';
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title><?php echo ENV; ?> | My Site</title>
+    <link rel="stylesheet" href="./assets/css/normalize.css" />
+    <link rel="stylesheet" href="./assets/css/main.css" />
+  </head>
+  <body>
+    <header>
+      <h1>My Site</h1>
 
-// Include the view file
-require_once 'app/views/index.php';
+      <nav>
+        <ul>
+          <li><a href="./index.php">Home</a></li>
+          <li><a href="./table.php">Table</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      <h2>Home</h2>
+      <p>Welcome to my site!</p>
+      <p>Test for git hokus</p>
+      <p>
+        <?php echo "Environment: " . ENV; ?>
+      </p>
+    </main>
+
+    <footer>
+      <p>My Site &copy; 2022</p>
+    </footer>
+  </body>
+</html>
