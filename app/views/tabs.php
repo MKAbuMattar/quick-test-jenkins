ac6b18c3300f70a18450cbfcc87cb2d72ddea697
MKAbuMattar <mohammad.khaled@outlook.com>
2022-12-07 20:55:37 +0300

    build: test the Jenkins job Automatically on GitHub push

--- app/views/tabs.php
+++ app/views/tabs.php
@@ -25,6 +25,25 @@
       <p>
         <?php echo "Environment: " . ENV; ?>
       </p>
+
+      <table border="1">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach ($users as $user) : ?>
+            <tr>
+              <td><?php echo $user['id']; ?></td>
+              <td><?php echo $user['name']; ?></td>
+              <td><?php echo $user['email']; ?></td>
+            </tr>
+          <?php endforeach; ?>
+        </tbody>
+      </table>
     </main>
 
     <footer>
