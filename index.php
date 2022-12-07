3fce30a26e9bf98a44fd10a01585f1450b15047b
MKAbuMattar <mohammad.khaled@outlook.com>
2022-12-07 19:52:20 +0300

    build: index.php text change

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
 // Include the controller file
 require_once 'app/controllers/index.php';
 
-// // Include the view file
-// require_once 'app/views/index.php';
+// Include the view file
+require_once 'app/views/index.php';
